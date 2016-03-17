009517cc069c650ebd1ece31c3edc68b592e6ded
Miguel Mendes <zephiro309@gmail.com>
2016-03-17 15:39:27 +0000

    First commit.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject cam = gameObject.transform.FindChild("Main Camera").gameObject;
+        if (Input.GetAxis("Vertical") > 0)
+            gameObject.transform.position += cam.transform.forward / 10;
+        else if (Input.GetAxis("Vertical") < 0)
+            gameObject.transform.position -= cam.transform.forward / 10;
+        else if (Input.GetAxis("Horizontal") > 0)
+            gameObject.transform.position += cam.transform.right / 10;
+        else if (Input.GetAxis("Horizontal") < 0)
+            gameObject.transform.position -= cam.transform.right / 10; ;
+    }
+}
