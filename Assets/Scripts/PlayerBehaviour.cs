5be00b615df5641065a98099e936110baf56593c
Miguel Mendes <zephiro309@gmail.com>
2016-06-02 11:22:05 +0100

    Significant puzzle progress.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -11,7 +11,7 @@ public class PlayerBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0)) {
-            Ray ray = new Ray(this.transform.GetComponentInChildren<Camera>().transform.position, this.transform.GetComponentInChildren<Camera>().transform.forward);
+            Ray ray = new Ray(this.transform.GetComponentInChildren<Camera>().transform.position + this.transform.GetComponentInChildren<Camera>().transform.forward, this.transform.GetComponentInChildren<Camera>().transform.forward);
             RaycastHit hit = new RaycastHit();
             if (Physics.Raycast(ray, out hit, 2)) {
                 if (hit.collider.transform.root.tag == "Door") {
